d323874f9269574b7191ba6c4af987d16b7eeaa8
Tim Hawes <trhawes@gmail.com>
2018-11-15 12:35:36 -0500

    Progress

---
+++ java/isogram/src/main/java/IsogramChecker.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+class IsogramChecker {
+    
+    boolean isIsogram(String phrase) {
+        boolean ret = true;
+        List<String> mylist = new ArrayList<String>(Arrays.asList(phrase.toLowerCase().split("")));
+        for (int i = 0; i < mylist.size(); i++) {
+            if (mylist.indexOf(mylist.get(i)) != mylist.lastIndexOf(mylist.get(i)))
+                ret = false;
+        }
+        return ret;
+    }
+}
