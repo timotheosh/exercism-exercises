5bf27ca01e023982038a5211cb0ab77eab9d46f1
Tim Hawes <tim@selfdidactic.com>
2018-11-18 16:46:29 -0500

    code improvement

--- racket/difference-of-squares/difference-of-squares.rkt
+++ racket/difference-of-squares/difference-of-squares.rkt
@@ -1,13 +1,17 @@
-#lang racket
+#lang racket/base
 
 (provide sum-of-squares square-of-sum difference)
 
+;; The function sqr is unavailable in racket/base, so we'll use our own.
+(define (square n)
+  (* n n))
+
 (define (square-of-sum num)
-  (sqr (for/sum ([i (in-range (add1 num))]) i)))
+  (square (for/sum ([i (in-range (add1 num))]) i)))
 
 (define (sum-of-squares num)
   (for/sum ([i (in-range (add1 num))])
-    (sqr i)))
+    (square i)))
 
 (define (difference num)
   (- (square-of-sum num) (sum-of-squares num)))
