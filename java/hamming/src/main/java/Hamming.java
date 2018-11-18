92515dea280ce22af30711200c8fe01155c5da41
Tim Hawes <tim@selfdidactic.com>
2018-11-18 16:37:01 -0500

    progress

--- java/hamming/src/main/java/Hamming.java
+++ java/hamming/src/main/java/Hamming.java
@@ -1,11 +1,22 @@
+import java.util.stream.IntStream;
+
 class Hamming {
 
+    String lStrand;
+    String rStrand;
+
     Hamming(String leftStrand, String rightStrand) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        if (leftStrand.length() != rightStrand.length()) {
+            throw new IllegalArgumentException("leftStrand and rightStrand must be of equal length.");
+        }
+        lStrand = leftStrand;
+        rStrand = rightStrand;
     }
 
     int getHammingDistance() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return IntStream.range(0, lStrand.length())
+                .map(i -> lStrand.charAt(i) != rStrand.charAt(i) ? 1: 0)
+                .sum();
     }
 
 }
