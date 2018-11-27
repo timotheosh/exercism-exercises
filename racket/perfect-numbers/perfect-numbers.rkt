c3d1da5d301cd62c60819b4352d77fd1f7ff5c20
Tim Hawes <trhawes@gmail.com>
2018-11-27 13:35:28 -0500

    Modify perfect-numbers exercise for latest test case.

--- racket/perfect-numbers/perfect-numbers.rkt
+++ racket/perfect-numbers/perfect-numbers.rkt
@@ -1,18 +1,13 @@
 #lang racket
 
-(provide perfect-numbers)
+(provide classify)
 
 
-(define (perfect? n)
-  (= n
-     (let ([half (add1 (quotient n 2))])
-       (for/sum ([x (in-range 1 half)])
-         (if (zero? (modulo n x))
-             x
-             0)))))
-
-
-(define (perfect-numbers n)
-  (for/list ([i (in-range 1 (add1 n))]
-             #:when (perfect? i))
-    i))
+(define (classify n)
+  (define sum
+    (for/sum ([x (in-range 1 (add1 (quotient n 2)))]
+              #:when (zero? (modulo n x)))
+      x))
+  (cond ((= n sum) 'perfect)
+        ((> n sum) 'deficient)
+        (else      'abundant)))
