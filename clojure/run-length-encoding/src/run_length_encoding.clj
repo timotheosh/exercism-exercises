a303199f6d39ba1beb2d5149ddebb989b7aba2f9
Tim Hawes <tim@selfdidactic.com>
2018-11-28 20:09:33 -0500

    progress

--- clojure/run-length-encoding/src/run_length_encoding.clj
+++ clojure/run-length-encoding/src/run_length_encoding.clj
@@ -3,9 +3,16 @@
 (defn run-length-encode
   "encodes a string with run-length-encoding"
   [plain-text]
-  )
+  (->>
+   (partition-by identity plain-text)
+   (map #(str (if (not (= 1 (count %))) (count %)) (first %)))
+   (apply str)))
 
 (defn run-length-decode
   "decodes a run-length-encoded string"
   [cipher-text]
-  )
+  (->>
+   (re-seq #"\d*\D|\D" cipher-text)
+   (map #(re-seq #"\d+|\D" %))
+   (map #(if (= (count %) 1) (apply str %) (apply str (repeat (Integer. (first %)) (second %)))))
+   (apply str)))
