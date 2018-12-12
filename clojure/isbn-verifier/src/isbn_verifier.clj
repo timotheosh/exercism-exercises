063c0bb54448bb601b604c50267d8b40ebd0b77a
Tim Hawes <trhawes@gmail.com>
2018-12-12 12:25:57 -0500

    Update isbn-verifier

--- clojure/isbn-verifier/src/isbn_verifier.clj
+++ clojure/isbn-verifier/src/isbn_verifier.clj
@@ -1,32 +1,13 @@
 (ns isbn-verifier)
 
-(defn dash?
-  "Predicate function that returns true if string is a dash."
-  [string]
-  (= string "-"))
-
-(defn valid-number?
-  "Returns true if an ISBN number contains all valid numbers and a valid check character."
-  [isbn]
-  (let [seq (reverse
-             (map read-string
-                  (remove isbn-verifier/dash?
-                          (clojure.string/split isbn #""))))]
-    (and (= (count seq) 10)
-         (empty? (remove number? (rest seq)))
-         (or (number? (first seq))
-             (= (first seq) 'X)))))
-
-(defn isbn?
-  "Checks if given string isbn is a valid ISBN-10 number."
-  [isbn]
-  (if (and (valid-number? isbn)
-           (let [seq (map (fn [x] (if (= x 'X) 10 x))
-                          (map read-string
-                               (remove isbn-verifier/dash?
-                                       (clojure.string/split isbn #""))))]
-             (zero? (mod (apply +
-                                (map (fn [x y] (* x y)) seq (range 10 0 -1)))
-                         11))))
-    true
-    false))
+(defn isbn? [isbn]
+  (->
+   (some->> (clojure.string/replace isbn "-" "")
+            (re-matches #"^\d{9}[\dX]$")
+            (map (fn [c] (if (= \X c) 10 (Character/digit c 10))))
+            (map (fn [n c] (* n c)) (range 10 0 -1))
+            (apply +)
+            (#(mod % 11))
+            zero?)
+   ;; No falsey's allowed, must be coerced into boolean.
+   boolean))
