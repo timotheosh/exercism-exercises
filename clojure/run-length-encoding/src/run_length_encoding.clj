e6b891684186b26c592dec7f9bf74719cf5b8d36
Tim Hawes <tim@selfdidactic.com>
2018-12-14 09:04:55 -0500

    Make run-length-encoding more clear.

--- clojure/run-length-encoding/src/run_length_encoding.clj
+++ clojure/run-length-encoding/src/run_length_encoding.clj
@@ -3,16 +3,16 @@
 (defn run-length-encode
   "encodes a string with run-length-encoding"
   [plain-text]
-  (->>
-   (partition-by identity plain-text)
-   (map #(str (when-not (= 1 (count %)) (count %)) (first %)))
-   (apply str)))
+  (->> plain-text
+       (partition-by identity)
+       (map #(str (when-not (= 1 (count %)) (count %)) (first %)))
+       (apply str)))
 
 (defn run-length-decode
   "decodes a run-length-encoded string"
   [cipher-text]
-  (->>
-   (re-seq #"\d*\D|\D" cipher-text)
-   (map #(re-seq #"\d+|\D" %))
-   (map #(if (= (count %) 1) (apply str %) (apply str (repeat (Integer. (first %)) (second %)))))
-   (apply str)))
+  (->> cipher-text
+       (re-seq #"\d*\D|\D")
+       (map #(re-seq #"\d+|\D" %))
+       (map #(if (= (count %) 1) (apply str %) (apply str (repeat (Integer. (first %)) (second %)))))
+       (apply str)))
