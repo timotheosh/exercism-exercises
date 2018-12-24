d392d9be569c1a8f5e07a674cf201feafd3e61bf
Tim Hawes <tim@selfdidactic.com>
2018-12-24 07:03:12 -0500

    Updated rna-transcription

--- common-lisp/rna-transcription/dna.lisp
+++ common-lisp/rna-transcription/dna.lisp
@@ -4,22 +4,11 @@
   (:export #:to-rna))
 (in-package #:dna)
 
-(defun string-to-list (str)
-  (assert (stringp str) (str))
-  (let ((s (string-upcase str)))
-    (coerce s 'list)))
-
-(defun list-to-string (lst)
-  (assert (or (listp lst)
-              (vectorp lst)) (lst))
-  (coerce lst 'string))
-
 (defun to-rna (strand)
   "Transcribe a string representing DNA nucleotides to RNA."
-  (list-to-string
-   (mapcar (lambda (x)
-             (case x
-               (#\G #\C)
-               (#\C #\G)
-               (#\T #\A)
-               (#\A #\U))) (string-to-list strand))))
+  (map 'string (lambda (x)
+                 (case x
+                   (#\G #\C)
+                   (#\C #\G)
+                   (#\T #\A)
+                   (#\A #\U))) strand))
