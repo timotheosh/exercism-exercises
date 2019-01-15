e8a428b8ce55f5e850a3a7bbacc62fe32061c278
Tim Hawes <trhawes@gmail.com>
2019-01-15 11:45:11 -0500

    progress

--- clojure/hamming/src/hamming.clj
+++ clojure/hamming/src/hamming.clj
@@ -1,5 +1,5 @@
 (ns hamming)
 
-(defn distance [strand1 strand2] ; <- arglist goes here
-  ;; your code goes here
-)
+(defn distance [strand1 strand2]
+  (when (= (count strand1) (count strand2))
+    (get (frequencies (map identical? strand1 strand2)) false 0)))
