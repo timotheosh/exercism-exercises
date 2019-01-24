b7f4b7039095cec81bffe0a8207b7585df909017
Tim Hawes <tim@selfdidactic.com>
2019-01-23 20:37:10 -0500

    Improvement on nucleotide-count for Racket

--- racket/nucleotide-count/nucleotide-count.rkt
+++ racket/nucleotide-count/nucleotide-count.rkt
@@ -2,22 +2,9 @@
 
 (provide nucleotide-counts)
 
-(define (valid-strand? ch)
-  (if (or (char=? ch #\A)
-          (char=? ch #\C)
-          (char=? ch #\G)
-          (char=? ch #\T))
-      #t
-      (error "invalid nucleotide")))
-
-(define (incr-assoc key alist)
-  (let ((pos (index-where alist (lambda (x) (char=? (car x) key))))
-        (value (cons key (add1 (cdr (assoc key alist))))))
-    ;;(displayln (format "pos: ~a val: ~a" pos value))
-    (list-set alist pos value)))
-
 (define (nucleotide-counts str)
-  (let ((new-list '((#\A . 0) (#\C . 0) (#\G . 0) (#\T . 0))))
-    (map (lambda (x) (when (valid-strand? x)
-                       (set! new-list (incr-assoc x new-list)))) (string->list str))
-    new-list))
+  (sort (hash->list
+         (for/fold ([new-list #hasheq((#\A . 0) (#\C . 0) (#\G . 0) (#\T . 0))])
+                   ([key (in-string str)])
+           (hash-update new-list key add1)))
+        char<? #:key car))
