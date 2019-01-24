1d7b6e4acc471f570ebad5b61162b535b91386c6
Tim Hawes <tim@selfdidactic.com>
2019-01-23 20:39:53 -0500

    Improvement on nucleotide-count for Racket

--- racket/nucleotide-count/nucleotide-count.rkt
+++ racket/nucleotide-count/nucleotide-count.rkt
@@ -2,9 +2,8 @@
 
 (provide nucleotide-counts)
 
-(define (nucleotide-counts str)
-  (sort (hash->list
-         (for/fold ([new-list #hasheq((#\A . 0) (#\C . 0) (#\G . 0) (#\T . 0))])
-                   ([key (in-string str)])
-           (hash-update new-list key add1)))
-        char<? #:key car))
+(define (nucleotide-counts dna)
+  (for/fold
+      ([acc '((#\A . 0) (#\C . 0) (#\G . 0) (#\T . 0))])
+      ([n dna])
+    (dict-update acc n add1)))
