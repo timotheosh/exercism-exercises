2174a5f846a3f4ee63a1d257effdccfb6f952bf0
Tim Hawes <tim@selfdidactic.com>
2019-01-27 08:31:30 -0500

    Improve on leap-year exercise for Common Lisp

--- common-lisp/leap/leap-year.lisp
+++ common-lisp/leap/leap-year.lisp
@@ -3,11 +3,9 @@
   (:export #:leap-year-p))
 (in-package #:leap)
 
-(defun evenly-divisible (x y)
-  (zerop (mod x y)))
-
 (defun leap-year-p (year)
-  (flet ((year-divisible-by (n) (evenly-divisible year n)))
+  (flet ((year-divisible-by (n)
+           (zerop (mod year n))))
     (and (year-divisible-by 4)
          (or (not (year-divisible-by 100))
              (year-divisible-by 400)))))
