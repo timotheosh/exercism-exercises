39681ab28aed6e00bfa28aeaf0e548ca5562bcde
Tim Hawes <tim.hawes@genesys.com>
2019-03-11 11:39:39 -0400

    Improve on isbn-verifier.

--- clojure/isbn-verifier/src/isbn_verifier.clj
+++ clojure/isbn-verifier/src/isbn_verifier.clj
@@ -1,13 +1,27 @@
 (ns isbn-verifier)
 
-(defn isbn? [isbn]
-  (->
-   (some->> (clojure.string/replace isbn "-" "")
-            (re-matches #"^\d{9}[\dX]$")
-            (map (fn [c] (if (= \X c) 10 (Character/digit c 10))))
-            (map (fn [n c] (* n c)) (range 10 0 -1))
-            (apply +)
-            (#(mod % 11))
-            zero?)
-   ;; No falsey's allowed, must be coerced into boolean.
-   boolean))
+(defn- hygenize-isbn
+  "Returns the match if an ISBN number contains all valid numbers and a valid check character."
+  [isbn]
+  (when (re-matches #"\d-?\d\d\d-?\d\d\d\d\d-?[\d|X]" isbn)
+    (clojure.string/replace isbn #"-" "")))
+
+(defn- isbn->vec
+  "Converts an isbn string into a list of numbers for our calculation."
+  [isbn]
+  (let [lst (mapv read-string (clojure.string/split isbn #""))]
+    (if (= 'X (last lst))
+      (assoc lst (dec (count lst)) 10)
+      lst)))
+
+(defn isbn?
+  "Checks if given string isbn is a valid ISBN-10 number."
+  [isbn]
+  (let [h-isbn (hygenize-isbn isbn)]
+    (if (and h-isbn
+             (zero? (mod (reduce + (map (fn [x y] (* x y))
+                                        (isbn->vec h-isbn)
+                                        (range 10 0 -1)))
+                         11)))
+      true
+      false)))
