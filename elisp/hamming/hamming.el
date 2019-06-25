6f967c1b5b7f00f093e32343cf41fbe3658eb810
Tim Hawes <tim.hawes@genesys.com>
2019-06-25 10:35:51 -0400

    progress

--- elisp/hamming/hamming.el
+++ elisp/hamming/hamming.el
@@ -3,13 +3,14 @@
 ;;; Commentary:
 
 ;;; Code:
-(require 'cl)
+(require 'cl-lib)
 
 (defun hamming-distance (strand1 strand2)
   "Number of positional differences in two equal length dna strands."
   (if (= (length strand1) (length strand2))
-      (count t
-             (map 'list (lambda(a b) (not (char-equal a b))) strand1 strand2))
+      (cl-loop for x across strand1
+               for y across strand2
+               count (not (= x y)))
     (error "strands are not the same length!")))
 
 (provide 'hamming)
