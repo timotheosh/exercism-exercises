c0a7179a29de04e119a0b252693d7aa1f8247b31
Tim Hawes <tim@selfdidactic.com>
2019-07-04 21:35:09 -0400

    Improvements to reverse-string exercise for rust

--- rust/reverse-string/src/lib.rs
+++ rust/reverse-string/src/lib.rs
@@ -1,7 +1,5 @@
-extern crate unicode_segmentation;
-
 use unicode_segmentation::UnicodeSegmentation;
 
 pub fn reverse(input: &str) -> String {
-    UnicodeSegmentation::graphemes(input, true).rev().flat_map(|g| g.chars()).collect()
+    input.graphemes(true).rev().collect()
 }
