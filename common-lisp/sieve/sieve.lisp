e6e9f31c2475538d0293665bb4e79fffba1364b5
Tim Hawes <trhawes@gmail.com>
2023-05-01 10:54:38 -0400

    Update sieve exercise for common lisp

--- common-lisp/sieve/sieve.lisp
+++ common-lisp/sieve/sieve.lisp
@@ -1,11 +1,10 @@
-(defpackage #:sieve
-  (:use #:cl)
-  (:export #:primes-to)
+(defpackage :sieve
+  (:use :cl)
+  (:export :primes-to)
   (:documentation "Generates a list of primes up to a given limit."))
 
-(in-package #:sieve)
+(in-package :sieve)
 
-(defun primes-to (limit)
-  (loop for x from 2 to limit
-       #:when (or (= x 2) (oddp x))
-     collect x))
+(defun primes-to (n)
+  "List primes up to `n' using sieve of Eratosthenes."
+  )
